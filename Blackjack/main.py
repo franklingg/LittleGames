179a5781572ab11d69aa07175fc89bbcc9e720ea
Franklin <franklin.oliveira@ccc.ufcg.edu.br>
2020-03-22 10:18:18 -0300

    First games

---
+++ Blackjack/main.py
@@ -0,0 +1,89 @@
+# Jogo de Blackjack entre 1 jogador e um dealer automático
+
+from blackjack_tools import game_functions as tools
+
+
+# initial variables
+dealer = 'Dealer'
+player = 'Jogador'
+bet_min = 20
+balance = 200
+
+# starting the game
+print("\nBem-vindo ao Super Cassino de Chernobyl! Hoje temos um ótimo Blackjack para você.")
+
+while True:
+    start = tools.corrected_input("\n######  START MENU  #######\n"
+                                  "(1) Começar a jogar\n"
+                                  "(2) Tutorial de Blackjack\n"
+                                  "(3) Configurações\n"
+                                  "Sua escolha: ", 1, 3)
+    if start == 1:
+        break
+    elif start == 2:
+        print(
+            "\n##########################################  TUTORIAL  ######################################################\n"
+            "Blackjack é o jogo mais famoso do mundo em cassinos. O objetivo é pontuar, com cartas, o mais próximo de 21, \n"
+            "sem estourar. Cada jogador recebe inicialmente 2 cartas (onde K,Q,J valem 10 pontos; o Ás vale 1 ou 11), o \n"
+            "Dealer tem uma carta à mostra e uma escondida. Em cada jogada, o jogador pode: a) manter suas cartas (se \n"
+            "acreditar que pode estourar; b) pedir mais uma carta ao dealer; c) render-se (e recuperar 50% da sua aposta;\n"
+            "d) dobrar a aposta (só é válido para o primeiro movimento e o jogador somente pode receber mais uma carta;\n"
+            "e) dividir (se começar com duas cartas iguais, pode fazer dois jogos independentes (com outra aposta).")
+    else:
+        configuration = tools.corrected_input("\n##############  SETTINGS  ##############\n"
+                                              "(1) Editar saldo inicial e aposta mínima\n"
+                                              "(2) Editar nomes\n"
+                                              "Sua escolha: ", 1, 2)
+        if configuration == 1:
+            balance = tools.corrected_input("Novo saldo: R$ ", 0, float('inf'))
+            bet_min = tools.corrected_input("Nova aposta mínima: R$ ", 0, balance)
+        elif configuration == 2:
+            dealer = input("Novo nome para o dealer: ")
+            player = input("Novo nome para o jogador: ")
+
+# starting rounds
+rounds = 1
+while True:
+    bet = tools.corrected_input(f"\nRodada {rounds}. Qual o valor da sua aposta (Aposta mínima: {bet_min})? R$ ", bet_min, balance, 'Aposta ou saldo insuficiente(s)')
+    balance -= bet
+    player_hand = tools.create_hand()
+    player_hand2 = ''
+    dealer_hand = tools.create_hand()
+    turn = 1
+    isThere2ndHand = False
+    end_check1 = False
+    end_check2 = False
+    while True:
+        if end_check1 and end_check2:
+            tools.show_cards(dealer_hand, dealer, player_hand, player, player_hand2, None)
+            break
+        elif end_check1:
+            tools.show_cards(dealer_hand, dealer, player_hand, player, player_hand2)
+            end_check2, second_hand, dealer_hand, player_hand2, balance = tools.round_menu(turn, dealer_hand, player_hand2, bet, balance, dealer, player)
+        elif end_check2:
+            tools.show_cards(dealer_hand, dealer, player_hand, player, player_hand2)
+            end_check1, second_hand, dealer_hand, player_hand, balance = tools.round_menu(turn, dealer_hand, player_hand, bet, balance, dealer, player)
+        else:
+            tools.show_cards(dealer_hand, dealer, player_hand, player, player_hand2)
+            end_check1, second_hand, dealer_hand, player_hand, balance = tools.round_menu(turn, dealer_hand, player_hand, bet, balance, dealer, player)
+
+            if second_hand:
+                player_hand2 = tools.create_hand()
+                isThere2ndHand = True
+            if isThere2ndHand:
+                end_check2, second_hand, dealer_hand, player_hand2, balance = tools.round_menu(turn, dealer_hand, player_hand2, bet, balance, dealer, player)
+            else:
+                end_check2 = True
+
+        turn += 1
+
+    tools.reshuffle_cards(dealer_hand, player_hand, player_hand2)
+
+    rounds += 1
+    lefting_game = tools.corrected_input(f"\n\nBoa partida!\n"
+                                         f"(1) Deseja jogar a rodada {rounds} ou\n"
+                                         f"(2) Deixar o jogo?\n"
+                                         f"Sua escolha: ", 1, 2)
+    if lefting_game == 2:
+        break
+print(f"Seu saldo final foi de R$ {balance}")
